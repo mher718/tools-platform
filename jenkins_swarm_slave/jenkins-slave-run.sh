0ed1d1c7eea734710018261486930f75daea3a62
alex-radionov <alexey.radionov@gmail.com>
2015-06-04 09:54:05 +0800

    jenkins-swarm-slave update
    Added executors parameter

--- jenkins_swarm_slave/jenkins-slave-run.sh
+++ jenkins_swarm_slave/jenkins-slave-run.sh
@@ -9,9 +9,10 @@ JENKINS_LABELS=${JENKINS_LABELS:-swarm}
 JENKINS_HOME=${JENKINS_HOME:-$HOME}
 JENKINS_USERNAME=${JENKINS_USERNAME:-user}
 JENKINS_PASSWORD=${JENKINS_PASSWORD:-password}
+JENKINS_EXECUTORS=${JENKINS_EXECUTORS:-executors}
 
 # start the docker daemon
 /usr/local/bin/wrapdocker &
 
 # start swarm slave
-java -jar /home/jenkins/swarm-client-1.22-jar-with-dependencies.jar -username $JENKINS_USERNAME -password $JENKINS_PASSWORD -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master $JENKINS_SERVER:$JENKINS_PORT $@
+java -jar /home/jenkins/swarm-client-1.22-jar-with-dependencies.jar -username $JENKINS_USERNAME -password $JENKINS_PASSWORD -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master $JENKINS_SERVER:$JENKINS_PORT -executors $JENKINS_EXECUTORS $@
